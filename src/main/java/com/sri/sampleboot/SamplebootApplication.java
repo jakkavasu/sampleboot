ba41a0a8274997838834ca17b6239f8c68f669cf
sjakka <jakkavasu@gmail.com>
2017-10-08 14:12:15 -0500

    Initial commit

---
+++ src/main/java/com/sri/sampleboot/SamplebootApplication.java
@@ -0,0 +1,12 @@
+package com.sri.sampleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplebootApplication.class, args);
+	}
+}
